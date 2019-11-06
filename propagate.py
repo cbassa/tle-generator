ed63243ad2f395a34c4d54b619526e7cb0a13dd0
Cees Bassa <cgbsat@gmail.com>
2019-11-06 17:04:15 +0100

    Added error checking

--- propagate.py
+++ propagate.py
@@ -41,7 +41,7 @@ def date2doy(year, mon, day, hr, minute, sec):
     
     return doy+fday
 
-def format_tle(coe, ep_year, ep_doy, sat, satno, desig, bstarstr):
+def format_tle_from_coe(coe, ep_year, ep_doy, sat, desig, bstarstr):
     # Radians to degrees
     r2d = 180.0/np.pi
     xpdotp = 1440.0/(2.0*np.pi)
@@ -53,34 +53,43 @@ def format_tle(coe, ep_year, ep_doy, sat, satno, desig, bstarstr):
     no = sat.whichconst.xke*(a/sat.whichconst.radiusearthkm)**(-3.0/2.0)*xpdotp
 
     # Format tle
-    tle1 = "1 %05dU %-8s %02d%12.8lf 0.00000000  00000-0 %8s 0    08" % (satno, desig,
+    tle1 = "1 %05dU %-8s %02d%12.8lf 0.00000000  00000-0 %8s 0    08" % (sat.satnum, desig,
                                                                            ep_year-2000, ep_doy,
                                                                            bstarstr)
-    tle2 = "2 %05d %8.4f %8.4f %07.0f %8.4f %8.4f %11.8lf    08" % (satno, incl*r2d,
+    tle2 = "2 %05d %8.4f %8.4f %07.0f %8.4f %8.4f %11.8lf    08" % (sat.satnum, incl*r2d,
                                                                     node*r2d, ecc*1e7,
                                                                     argp*r2d, m*r2d, no)
 
     return set_checksum(tle1), set_checksum(tle2)
 
-def propagate_tle(date, intle1, intle2, satno, desig, bstarstr):
+def propagate_tle(date, intle1, intle2):
     # Read TLE
     sat = twoline2rv(intle1, intle2, wgs84)
 
     # Get reference state vector
     r0, v0 = sat.propagate(*date)
+
+    # Exit if state vector contains nans
+    if np.isnan(np.array(r0+v0)).any():
+        return "", "", False
     
     # New epoch
     ep_year = date[0]
     ep_doy = date2doy(*date)
+
+    # Get info
+    desig = get_desig(intle1)
+    bstarstr = get_bstarstr(intle1)
 
     # Initial COE (classical orbital elements)
     coe_init = np.array(rv2coe(r0, v0, sat.whichconst.mu))
     coe_prev = coe_init
 
     # Format into TLE
-    tle1, tle2 = format_tle(coe_init, ep_year, ep_doy, sat, satno, desig, bstarstr)
+    tle1, tle2 = format_tle_from_coe(coe_init, ep_year, ep_doy, sat, desig, bstarstr)
 
     # Loop to iterate
+    converged = False
     for k in range(100):
         # Propagate
         sat = twoline2rv(tle1, tle2, wgs84)
@@ -102,7 +111,9 @@ def propagate_tle(date, intle1, intle2, satno, desig, bstarstr):
         if coe[3]<0.0:
             coe[3]*=-1
             coe[4]+=np.pi
-
+        if coe[3]>np.pi:
+            coe[3] = np.pi
+            
         # Keep eccentricity positive
         if coe[2]<0.0:
             coe[2] = 0.0
@@ -112,36 +123,36 @@ def propagate_tle(date, intle1, intle2, satno, desig, bstarstr):
             coe[i] = np.mod(coe[i], 2.0*np.pi)
             
         # Format into TLE
-        tle1, tle2 = format_tle(coe, ep_year, ep_doy, sat, satno, desig, bstarstr)
+        tle1, tle2 = format_tle_from_coe(coe, ep_year, ep_doy, sat, desig, bstarstr)
 
         # Save
         coe_prev = coe
 
-        # Exit on convergence
+        # Exit on convergence (dr in km, dv in km/s)
         if (dr<0.01) & (dv<0.001):
+            converged = True
             break
 
-    return tle1, tle2
+    return tle1, tle2, converged
 
-    
+def get_desig(tle1):
+    return tle1[9:17]
+
+def get_bstarstr(tle1):
+    return tle1[53:61]
+
 if __name__ == "__main__":
-    # Define TLE
-    tle = ["NOSS 3-6 (A)",
-           "1 38758U 12048A   19304.77896478 0.00000000  00000-0  00000-0 0    02",
-           "2 38758  63.4414 256.0512 0104915   1.4757 358.5242 13.40790232    00"]
+    with open("catalog.tle", "r") as fp:
+        lines = fp.readlines()
 
-    # Properties
-    satno = 38758
-    desig = "12048A"
-    bstarstr = " 00000-0"
-    
     # Propagate to
     date = [2019, 11, 6, 12, 0, 0]
 
     # Propagate
-    tle1, tle2 = propagate_tle(date, tle[1], tle[2], satno, desig, bstarstr)
+    for i in range(0, len(lines), 3):
+        tle1, tle2, converged = propagate_tle(date, lines[i+1], lines[i+2])
 
-    print(tle1)
-    print(tle2)
-    
+        print(lines[i].rstrip())
+        print(tle1)
+        print(tle2)
             
